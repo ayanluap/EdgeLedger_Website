036a0490962e044af3ef8a19a3d7531b9c46f439
ayanluap <ayanluap@gmail.com>
2020-02-01 13:23:12 +0530

    smooth scrolling updated

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body, #navbar a, .btn').animate({
-        scrollTop: $(hash).offset().top - 50
+        scrollTop: $(hash).offset().top - 100
       }, 900, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
